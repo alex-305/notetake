f11af564c2919614537d626e361807e6953fb87d
Alex Gonzalez <galex2003@icloud.com>
2023-11-29 13:54:41 -0500

    Added footer component

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@
 <div>
     <RouterView/>
 </div>
+<div>
+    <FooterComponent/>
+</div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import FooterComponent from './components/FooterComponent.vue';
 
 
 </script>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="footer">
+</div>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+.footer {
+    display: flex;
+}
+
+</style>
