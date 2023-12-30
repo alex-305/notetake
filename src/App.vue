5f6f92d4ef7e6c1854f28f880f2f2131be211dd2
Alex Gonzalez <galex2003@icloud.com>
2023-12-30 10:29:35 -0500

    Prettier changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-<div>
-</div>
-<div>
-    <RouterView/>
-</div>
-<div>
-    <FooterComponent/>
-</div>
+  <div></div>
+  <div>
+    <RouterView />
+  </div>
+  <div>
+    <FooterComponent />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import FooterComponent from './components/FooterComponent.vue';
-
-
+import FooterComponent from './components/FooterComponent.vue'
 </script>
