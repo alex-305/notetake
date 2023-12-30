ba1f74f6ab081b4532db4ba9e14dc38a84b38ea5
Alex Gonzalez <galex2003@icloud.com>
2023-12-30 10:36:55 -0500

    Save button now gives visual feedback as well as made changes to the way the title text is stored

--- src/components/NoteTitleBar.vue
+++ src/components/NoteTitleBar.vue
@@ -1,57 +1,77 @@
 <template>
-    <div class="noteTitleBar">
-        <button class="backButton" @click="$router.push('/')">⮜Back</button>
-        <input class="noteTitle" type="text" placeholder="New Note">
-        <button class="saveButton">Save🖫</button>
-    </div>
+  <div class="noteTitleBar">
+    <button class="backButton" @click="$router.push('/')">⮜Back</button>
+    <input name="title" v-model="title" class="noteTitle" type="text" placeholder="New Note" tabindex="1" />
+    <button class="plusButton saveButton" @click="save">{{ saveText }}</button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useCreateNoteStore } from '@/stores/CreateNoteStore'
+import { ref, watch } from 'vue'
+
+const noteStore = useCreateNoteStore()
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: ''
+  }
+})
+
+const title = ref(props.title);
+
+watch(title, (updatedText) => {
+    noteStore.setTitle(updatedText);
+})
 
 
+const saveText = ref('Save🖫')
+
+const save = () => {
+  saveButton()
+}
+
+const saveButton = () => {
+  saveText.value = 'Saved✔'
+  setTimeout(() => {
+    saveText.value = 'Save🖫'
+  }, 1000)
+}
 </script>
 
 <style>
 .noteTitle {
-    font-size: 30px;
-    width: 88%;
-    height: 35px;
-    background-color: #9fc4fc;
-    color: #000308;
-    border: none;
-    box-shadow: 0 4px 16px 0 rgba(0,0,0,0.1), 0 6px 20px 0 rgba(0,0,0,0.1);
+  font-size: 30px;
+  width: 88%;
+  height: 35px;
+  background-color: #9fc4fc;
+  color: #000308;
+  border: none;
+  box-shadow:
+    0 4px 16px 0 rgba(0, 0, 0, 0.1),
+    0 6px 20px 0 rgba(0, 0, 0, 0.1);
 }
 
 .noteTitle::placeholder {
-    color: #000308;
+  color: #000308;
 }
 
 .noteTitle:focus {
-    outline: none;
-    box-shadow: 0 4px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.2);
+  outline: none;
+  box-shadow:
+    0 4px 16px 0 rgba(0, 0, 0, 0.2),
+    0 6px 20px 0 rgba(0, 0, 0, 0.2);
 }
-
-.backButton:hover {
-    background-color: #FC9FC4;
-}
-
-.backButton:active:hover {
-    background-color: #BD7793;
-}
-
-.saveButton:hover {
-    background-color: #9FFCA9;
-}
-
-.saveButton:active:hover {
-    background-color: #77BD7F;
-}
-
 
 .noteTitleBar {
-    display: flex;
-    justify-content: space-between;
-    gap: 10px;
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+}
+
+.saveButton {
+  width: 70px;
 }
 
 </style>
