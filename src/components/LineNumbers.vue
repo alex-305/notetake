575984a205e785aee2b39d2edd37fb9627153f94
Alex Gonzalez <galex2003@icloud.com>
2023-12-30 10:32:33 -0500

    Added line numbers

--- src/components/LineNumbers.vue
+++ src/components/LineNumbers.vue
@@ -1,14 +1,44 @@
 <template>
-
-
+    <div>
+        <div v-for="line in lineNumbers" :key="line">
+            <button 
+            :class="{ active : line >=selectionRange[0] && line <=selectionRange[1]}"
+            >{{ line }}</button>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { toRefs } from 'vue';
 
+    const props = defineProps({
+        lineNumbers: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        selectionRange: {
+            type: Array as () => number[],
+            required: true
+        },
+    });
+    const { lineNumbers, selectionRange } = toRefs(props);
 </script>
 
 
-<style>
+<style scoped>
+button {
+    font-size: 20px;
+    margin: 0px;
+    padding: 0px;
+    width: 95%;
+    color: #8e979c;
+    background-color: #c3dafd;
+    box-shadow: none;
+}
 
+.active {
+    color: #2d3436;
+}
 
 </style>
--- src/components/NotePad.vue
+++ src/components/NotePad.vue
@@ -1,38 +1,56 @@
 <template>
-<div class="notePad" @keydown.ctrl="ctrlTextCommands">
+  <div class="notePad" @keydown.ctrl="ctrlTextCommands">
     <div>
-        <NoteTitleBar class="noteTitleBar"/>
+      <NoteTitleBar :title="title" class="noteTitleBar"/>
     </div>
-    <div>
-        <form class="textBox">
-            <TextBox class="textBox"/>
-        </form>
-    </div>
-</div>
+    <form class="textBox">
+      <NoteTextBox :text="text"/>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
-import TextBox from '../components/TextBox.vue';
-import NoteTitleBar from './NoteTitleBar.vue';
-import { ctrlTextCommands } from './TSFiles/commands';
+import NoteTextBox from '../components/NoteTextBox.vue'
+import NoteTitleBar from './NoteTitleBar.vue'
+import { ctrlTextCommands, saveNote } from './TSFiles/commands'
+import { onMounted } from 'vue'
 
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  text: {
+    type: String,
+    required: false,
+    default: ''
+  }
+})
+const title = props.title;
+const text = props.text;
+
+const save = () => {
+  (this as any).$refs.NoteTitleBar.save()
+  saveNote
+}
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@600&display=swap');
 
 .notePad {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .notePad * {
-    font-family: 'JetBrains Mono', monospace;
+  font-family: 'JetBrains Mono', monospace;
 }
 
 .notePad *::selection {
-    background-color: #A89FFC;
+  background-color: #a89ffc;
 }
 
 .textBox {
